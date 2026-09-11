62f44a62046a3eae898fd75e1696e47af6e62f94
mbrakatu <mabrakat@svsu.edu>
2026-09-11 01:29:00 -0400

    Update PWA cache
    Updated cache version and added activation event to clear old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,26 @@
-const CACHE_NAME = "my-schedule-cache-v1";
+const CACHE_NAME = "my-schedule-cache-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
